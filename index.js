2257edbdea7c73e2732e6727a6ee2b9d85ee829d
joaopedroaquino <aqerhack@gmail.com>
2022-10-04 16:12:03 -0300

    retornando os valores de endereco

--- index.js
+++ index.js
@@ -4,35 +4,72 @@
 2 OBTER O ENDEREÇO DE UM USUARIO PELO ID 
 */
 
-function obterUsuario(){
-    setTimeout(function(){
-        return{
+function obterUsuario(callback) {
+    setTimeout(function () {
+        return callback(null, {
             id: 1,
             nome: 'Joao Pedro',
             dataNascimento: new Date()
 
-        }
+        })
     }, 1000)
 }
 
-function obterTelefone(idUsuario){
+function obterTelefone(idUsuario, callback) {
 
-setTimeout(() => {
-    return{
-        telefone: '254453453',
-        ddd: 11
-    } 
-    
-}, 2000);
+    setTimeout(() => {
+        return callback(null, {
+            telefone: '254453453',
+            ddd: 11
+        })
 
- 
+    }, 2000);
+
+
 }
 
-function obterEndereco(idUsuario){}
+function obterEndereco(idUsuario, callback) {
+    setTimeout(() => {
+        return callback (null, {
+            rua:"rua teste",
+            numero: 12
+        }
+        )
+        
+    }, 2000);
+ }
+
+function resolverUsuario(erro, usuario) {
+    console.log('usuario', usuario)
 
 
-const usuario = obterUsuario()
+}
+
+
+obterUsuario(function resolverUsuario(error, usuario ) {
+    //null | "" | 0 == false
+    if(error){
+        console.error('deu ruim em usuario', error)
+        return;
+    }
+
+    obterTelefone(usuario.id, function resolverTelefone(error1,telefone){
+        if(error1){
+            console.error('deu ruim em telefone', error)
+            return;
+        }
+
+        obterEndereco(usuario.id, function resolverEndereco(error2, endereco){
+            if(error2){
+                console.error('deu ruim em telefone', error)
+                return;
+            }
+
+    })
+
+
+})
 // const telefone = obterTelefone(usuario.id)
 
-console.log('usuario', usuario)
 // console.log('telefone', telefone)
+})
