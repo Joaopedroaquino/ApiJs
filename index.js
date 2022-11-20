ea60bf18776ba8ca203a1f526cdabd715c090800
Joao Pedro Aquino <aqerhack@gmail.com>
2022-11-20 10:53:08 -0300

    add get

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json()); 
+const teste = [];
+
 
 server.get('/api',(req, res) => { return res.json({message: 'Hello World'})})
 
 
 server.listen(3000);
+
+
+server.get('/teste', (req, res) => {
+    return res.json(teste);
+})
+
+server.post('/teste',(req,res)=>{
+    const {name} = req.body
+    teste.push(name);
+    return res.json(teste)
+}
+)
